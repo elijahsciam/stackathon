308776dddb35dcb6ee2d34b2b4f61bbcbd5bb063
Elijah Sciammas <eli.sciammas@gmail.com>
2021-03-14 15:44:50 -0400

    Feat: click effects, natural/accidental keyboard, one premade song

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,15 @@ export class AppComponent {
   note: string;
   notes: Array<string>;
   grid: Array<string>;
+  grid2: Array<string>;
   gridNotes: string;
   time: Array<string>;
   dropdownItem: string;
   tempo: Array<number>;
   tempoItem: number;
   counter: number;
+  tone: Array<string>;
+  toneItem: string;
   constructor() {
     this.note = '';
     this.notes = [];
@@ -35,6 +38,39 @@ export class AppComponent {
       'A5',
       'B5',
       'C6',
+      'D6',
+      'E6',
+      'F6',
+      'G6',
+      'A6',
+      'B6',
+    ];
+    this.grid2 = [
+      'C4',
+      'Db4',
+      'D4',
+      'Eb4',
+      'E4',
+      'F4',
+      'F#4',
+      'G4',
+      'Ab4',
+      'A4',
+      'Bb4',
+      'B4',
+      'C5',
+      'Db5',
+      'D5',
+      'Eb5',
+      'E5',
+      'F5',
+      'F#5',
+      'G5',
+      'Ab5',
+      'A5',
+      'Bb5',
+      'B5',
+      'C6',
     ];
     this.gridNotes = '';
     this.time = ['4n', '2n', '1n', '8n'];
@@ -42,12 +78,16 @@ export class AppComponent {
     this.tempo = [0.3, 0.5, 0.7, 1, 1.5, 2];
     this.tempoItem = 1;
     this.counter = 0;
+    this.tone = ['1', '2', '3', '4'];
+    this.toneItem = '1';
   }
   changeValue(event: any) {
     const target = event.target.innerText;
     if (event.target.name === 'tempo') {
       this.tempoItem = Number(target);
       this.counter = 0;
+    } else if (event.target.name === 'tone') {
+      this.toneItem = target;
     } else {
       this.dropdownItem = target;
     }
@@ -72,8 +112,9 @@ export class AppComponent {
     this.gridNotes += ' ';
   }
   playAllGridNotes() {
-    const array = this.gridNotes.split(' ');
     const synth = new Tone.MonoSynth().toDestination();
+    const array = this.gridNotes.split(' ');
+
     synth.sync();
     for (let i = 0; i < array.length - 1; i++) {
       synth.triggerAttackRelease(array[i], this.dropdownItem, this.counter);
@@ -84,6 +125,48 @@ export class AppComponent {
       Tone.Transport.stop();
       synth.unsync();
     }, this.counter * 1000);
+  }
+  celine() {
+    const myHeart = [
+      'E4',
+      'F#4',
+      'B3',
+      'B4',
+      'A4',
+      'G#4',
+      'F#4',
+      'G#4',
+      'A4',
+      'G#4',
+      'F#4',
+      'E4',
+      'D#4',
+      'E4',
+      'D#4',
+      'C#4',
+      'B3',
+    ];
+    const synth = new Tone.MonoSynth().toDestination();
+    synth.sync();
+    synth.triggerAttackRelease(myHeart[0], '1n', 0);
+    synth.triggerAttackRelease(myHeart[1], '1n', 1);
+    synth.triggerAttackRelease(myHeart[2], '8n', 2);
+    synth.triggerAttackRelease(myHeart[3], '2n', 3);
+    synth.triggerAttackRelease(myHeart[4], '8n', 4);
+    synth.triggerAttackRelease(myHeart[5], '8n', 4.5);
+    synth.triggerAttackRelease(myHeart[6], '2n', 5);
+    synth.triggerAttackRelease(myHeart[7], '8n', 6);
+    synth.triggerAttackRelease(myHeart[8], '8n', 6.5);
+    synth.triggerAttackRelease(myHeart[9], '2n', 7);
+    synth.triggerAttackRelease(myHeart[10], '8n', 8);
+    synth.triggerAttackRelease(myHeart[11], '8n', 8.5);
+    synth.triggerAttackRelease(myHeart[12], '8n', 9);
+    synth.triggerAttackRelease(myHeart[13], '4n', 9.5);
+    synth.triggerAttackRelease(myHeart[14], '8n', 10.5);
+    synth.triggerAttackRelease(myHeart[15], '1n', 11);
+    synth.triggerAttackRelease(myHeart[16], '1n', 13);
+    Tone.Transport.start();
+    Tone.Transport.stop(20);
   }
 }
 
